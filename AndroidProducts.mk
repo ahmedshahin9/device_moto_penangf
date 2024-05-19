c7835298d2a961038a067a0d5c260fba9f5b261d
Ahmed Shahin <83367407+ahmedshahin9@users.noreply.github.com>
2024-05-19 23:20:05 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,10 +4,4 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_penangf.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_penangf-user \
-    lineage_penangf-userdebug \
-    lineage_penangf-eng
+PRODUCT_MAKEFILES := $(wildcard device/motorola/targets/devices/penangf/*.mk)
